24563572b5bf4b67b8eaea399c43185db704e73f
Leonardo Lira <leonardo.lira@hotmail.com.br>
2020-11-21 18:45:08 -0300

    create users and relation with appointment

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import { hash } from 'bcryptjs';
+import { getRepository } from 'typeorm';
+
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const hasUserSameEmail = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (hasUserSameEmail) {
+      throw new Error('Email address already used.');
+    }
+
+    const passwordHashed = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: passwordHashed,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
